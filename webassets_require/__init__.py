1ab2154bb007b5462ce93d2fe842f920b4f7800c
Bill Pullen <bill.pull1@gmail.com>
2013-07-11 14:43:58 -0400

    initial setup

---
+++ webassets_require/__init__.py
@@ -0,0 +1,10 @@
+from webassets.filter import Filter
+from webassets.filter import register_filter
+
+class RequireJSFilter(Filter):
+    name = "requireJS"
+    
+    def output(self, _in, out, **kwargs):
+        out.write(_in.read())
+
+register_filter(RequireJSFilter)        
