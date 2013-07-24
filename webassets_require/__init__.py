3eca687914217d640794b7065cd7c23eb12dbfd1
Johan Persson <johanopersson@gmail.com>
2013-07-24 16:12:45 +0200

    Output is now written to a temp file instead of being written to the output folder. After filter application the temp file is removed. This enables certain cache busting options without generating extra files.

--- webassets_require/__init__.py
+++ webassets_require/__init__.py
@@ -1,6 +1,8 @@
 from webassets.filter import Filter
 from webassets.filter import register_filter
 from RequireOptimizer import RequireOptimizer
+import tempfile
+import shutil
 
 
 class RequireJSFilter(Filter):
@@ -11,16 +13,20 @@ class RequireJSFilter(Filter):
         self.kwargs = kwargs
 
     def unique(self):
-        return self.args, self.kwargs
+        return [self.args].extend([(k, v) for (k, v) in self.kwargs.items()])
 
     def setup(self):
         pass
 
     def input(self, _in, out, **kwargs):
+        tmp_folder = tempfile.mkdtemp(prefix='webassets_require_')
+        tmp_output_path = '{tmp_folder}/optimized_file.js'.format(tmp_folder=tmp_folder)
+        
         source_path = kwargs.get('source_path')
-        output_path = kwargs.get('output_path')
         out.write(RequireOptimizer.run_input_optimizer(source_path,
-                                                       output_path,
+                                                       tmp_output_path,
                                                        **self.kwargs))
+        
+        shutil.rmtree(tmp_folder)
 
 register_filter(RequireJSFilter)
