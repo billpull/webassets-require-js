06c9a839d8695198c4ba1545eaca7ff8ab7b8306
Svante Paldan <svante.paldan@blocket.se>
2013-07-24 19:44:32 +0200

    Adding support for building with node instead of rhino. Node.js is now the default, which can be overridden by passing the use_rhino kwarg when creating the filter. Using node will require it to be installed on the machine already, and it should have the npm requires installed. Noted many hundred % speedup when running with node.js instead of Rhino.

--- webassets_require/RequireOptimizer.py
+++ webassets_require/RequireOptimizer.py
@@ -10,7 +10,6 @@ class OptimizationError(Exception):
 class RequireOptimizer(object):
     __REQUIRE_RESOURCES_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__),
                                               "lib"))
-    MB_MEMORY_FOR_JAVA = 256
 
     @staticmethod
     def _resource_path(file):
@@ -21,10 +20,13 @@ class RequireOptimizer(object):
     def run_input_optimizer(source_path, output_path, **kwargs):
         """ Run Optimizer Command and return file """
 
+        MB_MEMORY_FOR_JAVA = 256
+
         if not source_path.endswith(".js"):
             raise FilterError('requireJS: bundle file is not a js-file')
 
         base_url = kwargs.get('base_url', './')
+        use_rhino = kwargs.get('use_rhino', False)
 
         # Use the provided name or calculate the name relative to the base_url and minus the ".js"-part
         working_dir = os.getcwd()
@@ -37,20 +39,32 @@ class RequireOptimizer(object):
         for key, value in extra_args.iteritems():
             extra_arg_string += "%s=%s " % (key, value)
 
-        compiler_cmd = "java -Xmx{memory}m -classpath {rhino_js_jar}:{closure_compiler_jar} " \
-                        "org.mozilla.javascript.tools.shell.Main {require_js} " \
-                        "-o baseUrl={base_url} name={name} out={require_output_path} {require_extra_args}".format(memory=MB_MEMORY_FOR_JAVA,
-                                                                                                                  rhino_js_jar=RequireOptimizer._resource_path("js.jar"),
-                                                                                                                  closure_compiler_jar=RequireOptimizer._resource_path("compiler.jar"),
-                                                                                                                  require_js=RequireOptimizer._resource_path("r.js"),
-                                                                                                                  base_url=base_url,
-                                                                                                                  name=name,
-                                                                                                                  require_output_path=output_path,
-                                                                                                                  require_extra_args=extra_arg_string)
+        compiler_cmd = ''
+        if use_rhino:
+            compiler_cmd = "java -Xmx{memory}m -classpath {rhino_js_jar}:{closure_compiler_jar} " \
+                            "org.mozilla.javascript.tools.shell.Main {require_js} " \
+                            "-o baseUrl={base_url} name={name} out={require_output_path} {require_extra_args}".format(memory=MB_MEMORY_FOR_JAVA,
+                                                                                                                      rhino_js_jar=RequireOptimizer._resource_path("js.jar"),
+                                                                                                                      closure_compiler_jar=RequireOptimizer._resource_path("compiler.jar"),
+                                                                                                                      require_js=RequireOptimizer._resource_path("r.js"),
+                                                                                                                      base_url=base_url,
+                                                                                                                      name=name,
+                                                                                                                      require_output_path=output_path,
+                                                                                                                      require_extra_args=extra_arg_string)
+        else:
+            compiler_cmd = 'node {require_js} -o baseUrl={base_url} name={name} ' \
+                           'out={require_output_path} {require_extra_args}'.format(require_js=RequireOptimizer._resource_path("r.js"),
+                                                                                   base_url=base_url,
+                                                                                   name=name,
+                                                                                   require_output_path=output_path,
+                                                                                   require_extra_args=extra_arg_string)
 
         # Run the compiler.
         try:
-            os.system(compiler_cmd)
+            try:
+                os.system(compiler_cmd)
+            except OSError:
+                print "Unable to execute {0}, is it installed?".format('java' if use_rhino else 'node')
 
             fo = open(output_path, "r+")
 
